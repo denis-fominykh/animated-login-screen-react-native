9190a19be1b4a3020a56402a238d87532ba36eb6
denis-fominykh <fominykhdenis@gmail.com>
2020-07-13 19:26:35 +0300

    adding an app loading component

--- App.tsx
+++ App.tsx
@@ -1,8 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { Asset } from 'expo-asset';
+import { AppLoading } from 'expo';
+
+const cacheImages = (images) => {
+  return images.map((image) => {
+    if (typeof image === 'string') {
+      return Image.prefetch(image);
+    } else {
+      return Asset.fromModule(image).downloadAsync();
+    }
+  });
+};
 
 const App: FC = () => {
+  const [isReady, setIsReady] = useState<boolean>(false);
+
+  const _loadAssetsAsync = async () => {
+    const imageAssets = cacheImages([require('./assets/background.jpg')]);
+
+    await Promise.all([...imageAssets]);
+  };
+
+  if (!isReady) {
+    return (
+      <AppLoading
+        startAsync={_loadAssetsAsync}
+        onFinish={() => setIsReady(true)}
+        onError={console.warn}
+      />
+    );
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
