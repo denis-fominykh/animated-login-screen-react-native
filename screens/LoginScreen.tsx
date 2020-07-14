8bc84d6c350ec14ee481c18818e86b6a8ed8a567
denis-fominykh <fominykhdenis@gmail.com>
2020-07-14 17:47:57 +0300

    creating buttons' and background's animation

--- screens/LoginScreen.tsx
+++ screens/LoginScreen.tsx
@@ -1,28 +1,92 @@
-import React, { FC } from 'react';
-import { StyleSheet, View, Image, Dimensions, Text } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Image, Text, View, Dimensions } from 'react-native';
+import Animated from 'react-native-reanimated';
+import { State, TapGestureHandler } from 'react-native-gesture-handler';
 
-const LoginScreen: FC = () => {
-  return (
-    <View style={styles.container}>
-      <View style={styles.imageContainer}>
-        <Image
-          source={require('../assets/background.jpg')}
-          style={styles.image}
-        />
-      </View>
-      <View style={styles.buttonContainer}>
-        <View style={styles.button}>
-          <Text style={styles.buttonText}>SIGN IN</Text>
+import runTiming from '../services/runTimingServices';
+
+const {
+  Value,
+  event,
+  block,
+  cond,
+  eq,
+  set,
+  Clock,
+  interpolate,
+  Extrapolate,
+} = Animated;
+const { width, height } = Dimensions.get('window');
+
+class LoginScreen extends Component {
+  buttonOpacity = new Value(1);
+  onStateChange = event([
+    {
+      nativeEvent: ({ state }) =>
+        block([
+          cond(
+            eq(state, State.END),
+            set(this.buttonOpacity, runTiming(new Clock(), 1, 0)),
+          ),
+        ]),
+    },
+  ]);
+
+  buttonY = interpolate(this.buttonOpacity, {
+    inputRange: [0, 1],
+    outputRange: [100, 0],
+    extrapolate: Extrapolate.CLAMP,
+  });
+
+  backgroundY = interpolate(this.buttonOpacity, {
+    inputRange: [0, 1],
+    outputRange: [-height / 3, 0],
+    extrapolate: Extrapolate.CLAMP,
+  });
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Animated.View
+          style={{
+            ...styles.imageContainer,
+            transform: [{ translateY: this.backgroundY }],
+          }}
+        >
+          <Image
+            source={require('../assets/background.jpg')}
+            style={styles.image}
+          />
+        </Animated.View>
+        <View style={styles.buttonContainer}>
+          <TapGestureHandler onHandlerStateChange={this.onStateChange}>
+            <Animated.View
+              style={{
+                ...styles.button,
+                opacity: this.buttonOpacity,
+                transform: [{ translateY: this.buttonY }],
+              }}
+            >
+              <Text style={styles.buttonText}>SIGN IN</Text>
+            </Animated.View>
+          </TapGestureHandler>
+          <Animated.View
+            style={{
+              ...styles.button,
+              backgroundColor: '#2E71DC',
+              opacity: this.buttonOpacity,
+              transform: [{ translateY: this.buttonY }],
+            }}
+          >
+            <Text style={{ ...styles.buttonText, color: '#fff' }}>
+              SIGN IN WITH FACEBOOK
+            </Text>
+          </Animated.View>
         </View>
-        <View style={{ ...styles.button, backgroundColor: '#2E71DC' }}>
-          <Text style={{ ...styles.buttonText, color: '#fff' }}>
-            SIGN IN WITH FACEBOOK
-          </Text>
-        </View>
       </View>
-    </View>
-  );
-};
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -31,7 +95,7 @@ const styles = StyleSheet.create({
     justifyContent: 'flex-end',
   },
   imageContainer: {
-    ...StyleSheet.absoluteFill as {},
+    ...(StyleSheet.absoluteFill as {}),
   },
   image: {
     flex: 1,
